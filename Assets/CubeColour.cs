343dfae0f3103b5cc8d9dbfb1c980d5c4cc887b3
Jelle Fremery <j.fremery@betabit.nl>
2017-05-30 23:03:14 +0200

    adding files

---
+++ Assets/CubeColour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var newColour = new Color(Random.Range((float)0.0, (float)(1.0)), Random.Range((float)0.0, (float)(1.0)), Random.Range((float)0.0, (float)(1.0)));
+		this.GetComponent<Renderer>().material.color = newColour;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var x = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
